260857f9560264a63346326deb408b506938a572
Alex Shearn <alex.shearn@kraken.tech>
2024-10-04 10:40:18 +0100

    Adding skip/next

--- keyboards/dztech/dz60rgb_ansi/keymaps/shearn89/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/shearn89/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_FN1_LAYER] = LAYOUT_60_ansi(
     KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END, _______,
-    _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______, _______,
+    _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______, _______,
     _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______,
     _______, _______, _______, KC_MPLY, _______, _______, MO(_FN2_LAYER), _______
 ),
